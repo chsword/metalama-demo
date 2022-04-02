27baae3e17f9eb2af7794cf5cd89475a5e36144d
chsword <chsword@126.com>
2022-04-02 15:31:35 +0800

    示例1

---
+++ LogDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Metalama.Framework.Aspects;
+
+namespace LogDemo {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var r = Add(1, 2);
+            Console.WriteLine(r);
+        }
+        // 在这个方法中使用了下面的Attribute
+        [LogAttribute]
+        private static int Add(int a, int b)
+        {
+            var result = a + b;
+            Console.WriteLine("Add" + result);
+            return result;
+        }
+    }
+    // 这里是增加的 Attribute
+    public class LogAttribute : OverrideMethodAspect
+    {
+        public override dynamic? OverrideMethod()
+        {
+            Console.WriteLine(meta.Target.Method.ToDisplayString() + " 开始运行.");
+            var result = meta.Proceed();
+            Console.WriteLine(meta.Target.Method.ToDisplayString() + " 结束运行.");
+            return result;
+
+        }
+    }
+}
