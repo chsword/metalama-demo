6f3f8a3ef7880c7a9d2dfdfd99b6262c0eacede0
Zou Jian <chsword@126.com>
2022-04-06 23:01:20 +0800

    CodeFix & LiveTemplate

---
+++ src/ToStringWithCodeFix/ToStringAttribute.cs
@@ -0,0 +1,62 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Metalama.Framework.Code.SyntaxBuilders;
+using Metalama.Framework.CodeFixes;
+namespace ToStringWithCodeFix;
+
+public class ToStringAttribute : TypeAspect
+{
+    public override void BuildAspect(IAspectBuilder<INamedType> builder)
+    {
+        base.BuildAspect(builder);
+        // 添加一个建议手动实现的重构提示
+        if (builder.AspectInstance.Predecessors[0].Instance is IAttribute attribute)
+        {
+            builder.Diagnostics.Suggest(
+                new CodeFix("将 [ToString] 切换至手动实现", codeFixBuilder => this.ImplementManually(codeFixBuilder, builder.Target)),
+                builder.Target);
+        }
+    }
+
+    /// <summary>
+    /// 当点击手动实现时的操作
+    /// </summary>
+    private async Task ImplementManually(ICodeActionBuilder builder, INamedType targetType)
+    {
+        await builder.ApplyAspectAsync(targetType, this);
+        await builder.RemoveAttributesAsync(targetType, typeof(ToStringAttribute));
+    }
+
+    [Introduce(WhenExists = OverrideStrategy.Override, Name = "ToString")]
+    public string IntroducedToString()
+    {
+        // 获取非静态字段
+        var fields = meta.Target.Type.FieldsAndProperties.Where(f => !f.IsStatic).ToList();
+
+        // 构建一个$""字符串
+        var stringBuilder = new InterpolatedStringBuilder();
+        stringBuilder.AddText("{ ");
+        stringBuilder.AddText(meta.Target.Type.Name);
+        stringBuilder.AddText(" ");
+
+        var i = meta.CompileTime(0);
+
+        foreach (var field in fields)
+        {
+            if (i > 0)
+            {
+                stringBuilder.AddText(", ");
+            }
+
+            stringBuilder.AddText(field.Name);
+            stringBuilder.AddText("=");
+            stringBuilder.AddExpression(field.Invokers.Final.GetValue(meta.This));
+
+            i++;
+        }
+
+        stringBuilder.AddText(" }");
+
+        return stringBuilder.ToValue();
+    }
+}
